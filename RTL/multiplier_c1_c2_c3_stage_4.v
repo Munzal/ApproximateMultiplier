ef611eb4788a7da0f90d7db1ba45837cca58a334
Munzal <55911857+Munzal@users.noreply.github.com>
2023-05-02 00:02:29 +0530

    Add files via upload

---
+++ RTL/multiplier_c1_c2_c3_stage_4.v
@@ -0,0 +1,148 @@
+module HA(
+    output [1:0] op,
+    input [1:0] inp
+);
+    wire Sum, Carry;
+    wire A, B;
+    assign A = inp[0];
+    assign B = inp[1];
+    assign Sum = A&(~B) | (~A)&B;
+    assign Carry = A & B;
+    assign op = {Carry,Sum};
+endmodule
+
+module FA(
+    output [1:0] op,
+    input [2:0] inp
+);
+    wire I;
+    wire Sum, Carry;
+    wire A, B, C;
+    assign A = inp[0];
+    assign B = inp[1];
+    assign C = inp[2];
+    assign I = (~A)&B | A&(~B);
+    assign Sum = (~I)&C | I&(~C);
+    assign Carry = A&B | C&I;
+    assign op = {Carry,Sum};
+endmodule
+
+module comp4x2(
+    output [1:0] op,
+    input [3:0] inp
+);
+    wire w1, w2;
+
+    assign w1 = inp[0] | inp[1];
+    assign w2 = inp[2] | inp[3];
+
+    assign op[1] = w1 & w2;
+    assign op[0] = w1 | w2;
+
+endmodule
+
+module comp6x3(
+    output [2:0] op,
+    input [5:0] inp
+);
+    wire w1, w2, w3, w4, w5, w6;
+    wire a1, a2, a3, a4, a5, a6, a7;
+
+    assign w1 = inp[0] | inp[1] | inp[2];
+    assign w2 = inp[3] & inp[4] & inp[5];
+    assign w3 = inp[0] & inp[1] & inp[2];
+    assign w4 = inp[3] | inp[4] | inp[5];
+
+    assign w5 = w1 & w2;
+    assign w6 = w3 & w4;
+
+    assign op[2] = w5 | w6; //msb
+    assign op[0] = op[2]; //lsb
+
+    //mid bit
+    assign a1 = inp[0] | inp[1];
+    assign a2 = inp[2] | inp[3];
+    assign a3 = inp[4] | inp[5];
+
+    assign a4 = a1 & a2;
+    assign a5 = a2 & a3;
+    assign a6 = a3 & a1;
+
+    assign a7 = a4 | a5 | a6;
+
+    assign op[1] = a7 & (~op[2]);//mid bit 
+
+endmodule
+
+module comp8x3(
+    output [2:0] op,
+    input [7:0] inp
+);
+    wire w1, w2, w3, w4, w5;
+
+    comp4x2 c1({w2,w1},{inp[0],inp[1],inp[2],inp[3]});
+    comp4x2 c2({w4,w3},{inp[4],inp[5],inp[6],inp[7]});
+    HA      h1({w5,op[0]},{w3,w1});
+    FA      f1({op[2],op[1]},{w1,w2,w5});
+
+endmodule
+
+module multiplier_nov(
+    output [15:0] w,
+    input [7:0] num1,
+    input [7:0] num2
+);
+    wire a[7:0][7:0];
+    genvar i,j;
+
+    //level 1
+    generate
+        for(i=0; i<8; i=i+1)
+        begin
+            for(j=0; j<8; j=j+1)
+            begin
+                assign a[i][j] = num1[i] & num2[j];
+            end
+        end
+    endgenerate
+    //level 2
+
+    wire [2:0] level2 [0:14];// considering 3 bit output for all modules in level 1
+
+    HA          hor01(level2[0][1:0],{a[1][0], a[0][1]});
+    FA          hor02(level2[1][1:0],{a[0][2],a[2][0], a[1][1]});
+    comp4x2     hor03(level2[2][1:0],{a[3][0], a[1][2], a[2][1], a[0][3]});
+    comp6x3     hor04(level2[3][2:0],{a[4][0], a[1][3], a[3][1], a[0][4], a[2][2],1'b0});
+    comp6x3     hor05(level2[4][2:0],{a[5][0], a[0][5], a[1][4], a[4][1], a[3][2], a[2][3]});
+    comp8x3     hor06(level2[5][2:0],{a[6][0], a[0][6], a[1][5], a[5][1], a[2][4], a[4][2], a[3][3],1'b0});
+    comp8x3     hor07(level2[6][2:0],{a[7][0], a[0][7], a[1][6], a[6][1], a[2][5], a[5][2], a[4][3], a[3][4]});
+    comp8x3     hor08(level2[7][2:0],{a[7][1], a[1][7], a[2][6], a[6][2], a[5][3], a[3][5], a[4][4],1'b0});
+    comp6x3     hor09(level2[8][2:0],{a[7][2], a[2][7], a[3][6], a[6][3], a[5][4], a[4][5]});
+    comp6x3     hor10(level2[9][2:0],{a[7][3], a[3][7], a[4][6], a[6][4], a[5][5],1'b0});
+    comp4x2     hor11(level2[10][1:0],{a[7][4], a[4][7], a[5][6], a[6][5]});
+    FA          hor12(level2[11][1:0],{a[7][5], a[5][7], a[6][6]});
+    HA          hor13(level2[12][1:0],{a[7][6], a[6][7]});
+
+    assign level2[13][0] = a[7][7];
+
+    wire level3_carry [0:12];
+    wire [13:0] prod;
+
+    HA          hrr01({level3_carry[0], prod[0]}, {level2[0][1], level2[1][0]});
+    comp4x2     hrr02({level3_carry[1], prod[1]}, {level3_carry[0], level2[1][1], level2[2][0], 1'b0});
+    comp4x2     hrr03({level3_carry[2], prod[2]}, {level3_carry[1], level2[2][1], level2[3][0], 1'b0});
+    comp4x2     hrr04({level3_carry[3], prod[3]}, {level3_carry[2], level2[3][1], level2[4][0], 1'b0});
+    comp4x2     hrr05({level3_carry[4], prod[4]}, {level3_carry[3], level2[4][1], level2[5][0], 1'b0});
+    comp4x2     hrr06({level3_carry[5], prod[5]}, {1'b0, level2[5][1], level2[6][0], 1'b0});
+    comp4x2     hrr07({level3_carry[6], prod[6]}, {level3_carry[5], level2[6][1], level2[7][0], 1'b0});
+    comp4x2     hrr08({level3_carry[7], prod[7]}, {level3_carry[6], level2[7][1], level2[8][0], 1'b0});
+    comp4x2     hrr09({level3_carry[8], prod[8]}, {level3_carry[7], level2[8][1], level2[9][0], 1'b0});
+    comp4x2     hrr10({level3_carry[9], prod[9]}, {1'b0, level2[9][1], level2[10][0], 1'b0});
+    comp4x2     hrr11({level3_carry[10], prod[10]}, {level3_carry[9], level2[10][1], level2[11][0], 1'b0});
+    comp4x2     hrr12({level3_carry[11], prod[11]}, {level3_carry[10], level2[11][1], level2[12][0], 1'b0});
+    comp4x2     hrr13({level3_carry[12], prod[12]}, {level3_carry[11], level2[12][1], level2[13][0], 1'b0});
+
+    assign prod[13] = level3_carry[12];
+    assign w = {prod,level2[0][0],a[0][0]};
+    
+endmodule
---
+++ RTL/multiplier_c1_mwtm.v
@@ -0,0 +1,146 @@
+`timescale 1ns / 1ps
+`include "multiplier_pro.v"
+
+module HA(
+    output Sum,
+    output Carry,
+    input A,
+    input B
+);
+    assign Sum = A&(~B) | (~A)&B;
+    assign Carry = A & B;
+endmodule
+
+module FA(
+    output Sum,
+    output Carry,
+    input A,
+    input B,
+    input C
+);
+    wire I;
+    assign I = (~A)&B | A&(~B);
+    assign Sum = (~I)&C | I&(~C);
+    assign Carry = A&B | C&I;
+
+endmodule
+
+module comp4x2(
+    output sum_out,
+    output carry,
+    input x1,
+    input x2,
+    input x3,
+    input x4
+);
+    wire w1, w2;
+    wire [1:0] op;
+
+    assign w1 = x1 | x2;
+    assign w2 = x3 | x4;
+
+    assign op[1] = w1 & w2;
+    assign op[0] = w1 | w2;
+
+    assign carry = op[1];
+    assign sum_out = op[0];
+
+endmodule
+
+module multiplier_nov(
+    output [15:0] w,
+    input [7:0] num1,
+    input [7:0] num2
+);
+    wire a[7:0][7:0];
+    genvar i,j;
+
+    //level 1
+    generate
+        for(i=0; i<8; i=i+1)
+        begin
+            for(j=0; j<8; j=j+1)
+            begin
+                assign a[i][j] = num1[i] & num2[j];
+            end
+        end
+    endgenerate
+    //level 2
+    
+    wire p[7:0][7:0];
+    wire g[7:0][7:0];
+    assign p[2][1] = a[2][1] | a[1][2];
+    assign g[2][1] = a[2][1] & a[1][2];
+    generate
+        for(i=3; i<7; i=i+1)
+        begin
+            for(j=0; j<i; j=j+1)
+            begin
+                assign p[i][j] = a[i][j] | a[j][i];
+                assign g[i][j] = a[i][j] & a[j][i];
+            end
+        end
+    endgenerate
+    generate
+        for(j=0;j<5;j=j+1)
+        begin
+            assign p[7][j] = a[7][j] | a[j][7];
+            assign g[7][j] = a[7][j] & a[j][7];
+        end
+    endgenerate
+    
+    //level 3
+    wire r1[10:0], r2[10:0], r3[10:0];
+
+    assign r1[0] = p[3][0];
+    assign r2[0] = p[2][1];
+
+    HA          h1(r1[1], r3[2], p[4][0], p[3][1]);
+    FA          f1(r1[2], r3[3], p[5][0], p[4][1], p[3][2]);
+    comp4x2  c1(r1[3], r3[4], p[6][0], p[5][1], p[4][2], a[3][3]);
+    comp4x2  c2(r1[4], r3[5], p[7][0], p[6][1], p[5][2], p[4][3]);
+    comp4x2  c3(r1[5], r3[6], p[7][1], p[6][2], p[5][3], a[4][4]);
+    FA          f2(r1[6], r3[7], p[7][2], p[6][3], p[5][4]);
+    FA          f3(r1[7], r3[8], p[7][3], p[6][4], a[5][5]);
+    HA          h2(r1[8], r2[9], p[7][4], p[6][5]);
+    HA          h3(r1[9], r3[10],a[7][5], a[5][7]);
+
+    assign r1[10] = a[7][6];
+    assign r2[10] = a[6][7];
+    assign r3[9] = a[6][6];
+    assign r3[0] = g[3][0] | g[2][1];
+    assign r3[1] = g[4][0] | g[3][1];
+
+    assign r2[1] = a[2][2];
+    assign r2[2] = g[5][0] | g[4][1] | g[3][2];
+    assign r2[3] = g[6][0] | g[5][1] | g[4][2];
+    assign r2[4] = g[7][0] | g[6][1] | g[5][2] | g[4][3];
+    assign r2[5] = g[7][1] | g[6][2] | g[5][3];
+    assign r2[6] = g[7][2] | g[6][3] | g[5][4];
+    assign r2[7] = g[7][3] | g[6][4];
+    assign r2[8] = g[7][4] | g[6][5];
+    assign r2[10] = a[6][7];
+
+    //level 4
+    //wire w[14:0];
+    wire last_level[0:11];
+    assign w[0] = a[0][0];
+    assign w[1] = a[1][0] | a[0][1];//instead of Approx half adder we use OR gate
+    
+    comp4x2       fn1(w[2], last_level[0], a[2][0], a[0][2], a[1][1], 1'b0);
+    comp4x2       fn2(w[3], last_level[1], r1[0], r2[0], r3[0], last_level[0]);
+    comp4x2       fn3(w[4], last_level[2], r1[1], r2[1], r3[1], last_level[1]);
+    comp4x2       fn4(w[5], last_level[3], r1[2], r2[2], r3[2], last_level[2]);
+    comp4x2       fn5(w[6], last_level[4], r1[3], r2[3], r3[3], last_level[3]);
+    comp4x2       fn6(w[7], last_level[5], r1[4], r2[4], r3[4], last_level[4]);
+    comp4x2       fn7(w[8], last_level[6], r1[5], r2[5], r3[5], last_level[5]);
+    comp4x2       fn8(w[9], last_level[7], r1[6], r2[6], r3[6], last_level[6]);
+    comp4x2       fn9(w[10], last_level[8], r1[7], r2[7], r3[7], last_level[7]);
+    comp4x2       fn10(w[11], last_level[9], r1[8], r2[8], r3[8], last_level[8]);
+    comp4x2       fn11(w[12], last_level[10], r1[9], r2[9], r3[9], last_level[9]);
+    comp4x2       fn12(w[13], last_level[11], r1[10], r2[10], r3[10], last_level[10]);
+    
+    assign w[14] = a[7][7];
+    assign w[15] = 1'b0;
+
+endmodule
